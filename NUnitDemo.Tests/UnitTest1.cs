e12871d32b197212f9cc314c93445b95f41f8c44
Asad <aliasad07518@gmail.com>
2026-01-29 12:22:35 +0530

    Multiple exception

--- NUnitDemo.Tests/UnitTest1.cs
+++ NUnitDemo.Tests/UnitTest1.cs
@@ -14,39 +14,39 @@ public class CalculatorTests
         _calc = new Calculator();
     }
 
-    [Test]
-    public void All_Assertion_Examples()
-    {
-        // 1. Equality Assertions
-        Assert.That(_calc.Add(2, 2), Is.EqualTo(4));
-        Assert.That(_calc.Add(2, 2), Is.Not.EqualTo(5));
+  [Test]
+public void All_Assertion_Examples()
+{
+    // 1. Equality Assertions
+    int sum = _calc.Add(2, 2);
+    Assert.That(sum, Is.EqualTo(4));
+    Assert.That(sum, Is.Not.EqualTo(5));
 
-        // 2. Numeric Comparisons
-        Assert.That(10, Is.GreaterThan(5));
-        Assert.That(10, Is.LessThan(20));
-        Assert.That(10, Is.InRange(1, 100));
+    // 2. Numeric Comparisons
+    int number = _calc.Add(7, 3);
+    Assert.That(number, Is.GreaterThan(5));
+    Assert.That(number, Is.LessThan(20));
+    Assert.That(number, Is.InRange(1, 100));
 
-        // 3. Boolean & Nulls
-        Assert.That(true, Is.True);
-        Assert.That(false, Is.False);
-        Assert.That((string?)null, Is.Null);
-        Assert.That("test", Is.Not.Null);
+    // 3. Boolean & Nulls
+    string greeting = _calc.GetGreeting("Ali");
+    Assert.That(greeting, Is.Not.Null);
+    Assert.That(greeting.Contains("Ali"), Is.True);
 
-        // 4. String Assertions
-        string msg = "Hello World";
-        Assert.That(msg, Does.Contain("World"));
-        Assert.That(msg, Does.StartWith("Hello"));
-        Assert.That(msg, Does.EndWith("World"));
+    // 4. String Assertions
+    Assert.That(greeting, Does.StartWith("Hello"));
+    Assert.That(greeting, Does.EndWith("!"));
 
-        // 5. Collection Assertions
-        var list = new List<int> { 1, 2, 3 };
-        Assert.That(list, Has.Count.EqualTo(3));
-        Assert.That(list, Has.Member(2));
-        Assert.That(list, Is.Not.Empty);
+    // 5. Collection Assertions
+    var list = new List<int> { sum, number };
+    Assert.That(list, Has.Count.EqualTo(2));
+    Assert.That(list, Has.Member(4));
+    Assert.That(list, Is.Not.Empty);
 
-        // 6. Exception Assertions
-        Assert.Throws<ArgumentException>(() => _calc.Divide(10, 0));
-    }
+    // 6. Exception Assertions
+    Assert.Throws<ArgumentException>(() => _calc.Divide(10, 0));
+}
+
 
     // 7. Parameterized Tests (Running the same test with different data)
     [TestCase(1, 2, 3)]
