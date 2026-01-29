237db29b47eb545458fac6f0f625e2e9b6d19bcd
Asad <aliasad07518@gmail.com>
2026-01-29 12:05:39 +0530

    Fix: Change DivideByZeroException to ArgumentException

--- NUnitDemo.Core/Class1.cs
+++ NUnitDemo.Core/Class1.cs
@@ -1,13 +1,13 @@
 ﻿namespace NUnitDemo.Core;
-
 public class Calculator
 {
     public int Add(int a, int b) => a + b;
-
-    public int Divide(int a, int b)
+    
+    public double Divide(double a, double b)
     {
-        if (b == 0)
-            throw new ArgumentException("Cannot divide by zero", nameof(b));
+        if (b == 0) throw new ArgumentException("Cannot divide by zero.");
         return a / b;
     }
+
+    public string GetGreeting(string name) => $"Hello, {name}!";
 }
